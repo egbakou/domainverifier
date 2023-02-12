fec6857bd60aa1c81438a6d5b660cc7484f28ad8
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2023-02-12 13:22:49 +0100

    test(gen): added GenerateXml tests

--- generator_test.go
+++ generator_test.go
@@ -1,6 +1,7 @@
 package domainverifier
 
 import (
+	"errors"
 	"github.com/egbakou/domainverifier/config"
 	"strings"
 	"testing"
@@ -240,6 +241,139 @@ func TestGenerateJson(t *testing.T) {
 				t.Errorf("GenerateJson() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+			if got != nil && !strings.Contains(got.FileContent, tt.want.FileContent) {
+				t.Errorf("expected: %v, got: %v", tt.want.FileContent, got.FileContent)
+			}
+		})
+	}
+}
+
+func TestGenerateXmlFromConfig(t *testing.T) {
+	type args struct {
+		config          *config.XmlGenerator
+		useInternalCode bool
+	}
+	testCases := []struct {
+		name    string
+		args    args
+		want    *FileInstruction
+		wantErr error
+	}{
+		{
+			name: "Successful generation with external code",
+			args: args{
+				config: &config.XmlGenerator{
+					FileName: "example.xml",
+					RootName: "example-root",
+					Code:     "internal-code",
+				},
+				useInternalCode: false,
+			},
+			want: &FileInstruction{
+				FileName:    "example.xml",
+				FileContent: `<example-root><code>internal-code</code></example-root>`,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Successful generation with internal code",
+			args: args{
+				config: &config.XmlGenerator{
+					FileName: "example",
+					RootName: "example-root",
+				},
+				useInternalCode: true,
+			},
+			want: &FileInstruction{
+				FileName:    "example",
+				FileContent: `<example-root><code>`,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Validation error",
+			args: args{
+				config:          nil,
+				useInternalCode: true,
+			},
+			want:    nil,
+			wantErr: errors.New("config cannot be nil"),
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GenerateXmlFromConfig(tt.args.config, tt.args.useInternalCode)
+			if err == nil && tt.wantErr != nil {
+				t.Errorf("expected error: %v, got: %v", tt.wantErr, err)
+			}
+
+			if tt.wantErr == nil && err != nil {
+				t.Errorf("expected no error, got: %v", err)
+			}
+
+			if got != nil && !strings.Contains(got.FileContent, tt.want.FileContent) {
+				t.Errorf("expected: %v, got: %v", tt.want.FileContent, got.FileContent)
+			}
+		})
+	}
+}
+
+func TestGenerateXml(t *testing.T) {
+	type args struct {
+		appName         string
+		sanitizeAppName bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *FileInstruction
+		wantErr error
+	}{
+		{
+			name: "valid app name and sanitize is false",
+			args: args{
+				appName:         "my super app",
+				sanitizeAppName: false,
+			},
+			want: &FileInstruction{
+				FileName:    "my super app.xml",
+				FileContent: `<verification><code>`,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "valid app name and sanitize is true",
+			args: args{
+				appName:         "my super app",
+				sanitizeAppName: true,
+			},
+			want: &FileInstruction{
+				FileName:    "mysuperapp.xml",
+				FileContent: `<verification><code>`,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "invalid app name",
+			args: args{
+				appName: "",
+			},
+			want:    nil,
+			wantErr: InvalidAppNameError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GenerateXml(tt.args.appName, tt.args.sanitizeAppName)
+			if err == nil && tt.wantErr != nil {
+				t.Errorf("expected error: %v, got: %v", tt.wantErr, err)
+			}
+
+			if tt.wantErr == nil && err != nil {
+				t.Errorf("expected no error, got: %v", err)
+			}
+
 			if got != nil && !strings.Contains(got.FileContent, tt.want.FileContent) {
 				t.Errorf("expected: %v, got: %v", tt.want.FileContent, got.FileContent)
 			}
