bfa9c4e26c5495666418952b71081f39c0e88c54
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2023-02-12 13:22:10 +0100

    test(gen): added GenerateJson tests

--- generator_test.go
+++ generator_test.go
@@ -133,3 +133,116 @@ func TestGenerateHtmlMeta(t *testing.T) {
 		})
 	}
 }
+
+func TestGenerateJsonFromConfig(t *testing.T) {
+	type args struct {
+		config          *config.JsonGenerator
+		useInternalCode bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *FileInstruction
+		wantErr bool
+	}{
+		{
+			name: "Successful generation with external code",
+			args: args{
+				config: &config.JsonGenerator{
+					FileName:  "example.json",
+					Attribute: "code",
+					Code:      "external-code",
+				},
+				useInternalCode: false,
+			},
+			want: &FileInstruction{
+				FileName:    "example.json",
+				FileContent: `{"code": "external-code"}`,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Successful generation with internal code",
+			args: args{
+				config: &config.JsonGenerator{
+					FileName:  "example",
+					Attribute: "code",
+				},
+				useInternalCode: true,
+			},
+			want: &FileInstruction{
+				FileName:    "example",
+				FileContent: `{"code": "`,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Validation error",
+			args: args{
+				config:          nil,
+				useInternalCode: true,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GenerateJsonFromConfig(tt.args.config, tt.args.useInternalCode)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GenerateJsonFromConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != nil && !strings.Contains(got.FileContent, tt.want.FileContent) {
+				t.Errorf("expected: %v, got: %v", tt.want.FileContent, got.FileContent)
+			}
+		})
+	}
+}
+
+func TestGenerateJson(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    string
+		want    *FileInstruction
+		wantErr bool
+	}{
+		{
+			name: "valid app name and sanitize is false",
+			args: "my super app",
+			want: &FileInstruction{
+				FileName:    "my super app.json",
+				FileContent: `{"mysuperapp_site_verification": "`,
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid app name and sanitize is true",
+			args: "my super app",
+			want: &FileInstruction{
+				FileName:    "mysuperapp.json",
+				FileContent: `{"mysuperapp_site_verification": "`,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "invalid app name",
+			args:    "",
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GenerateJson(tt.args)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GenerateJson() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != nil && !strings.Contains(got.FileContent, tt.want.FileContent) {
+				t.Errorf("expected: %v, got: %v", tt.want.FileContent, got.FileContent)
+			}
+		})
+	}
+}
